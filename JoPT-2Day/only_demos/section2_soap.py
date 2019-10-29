11154fa13b868b3f7b2a537c28eba939c56e42ea
Rahul Verma <rahulverma81@gmail.com>
2019-10-29 19:07:29 +0530

    Added missing license headers.

--- JoPT-2Day/only_demos/section2_soap.py
+++ JoPT-2Day/only_demos/section2_soap.py
@@ -1,3 +1,21 @@
+'''
+This file is a part of The Joy of Python Gihub Repository.
+Copyright 2019 Rahul Verma
+Website: www.RahulVerma.net
+Email: rv [at] testmile.com
+Creator: Rahul Verma
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+  http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
+
 import requests
 import xml.etree.ElementTree as etree
 
